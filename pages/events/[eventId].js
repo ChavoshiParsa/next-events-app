8e40e832376b589313a1aa7faa1dcd0f757b7220
Your Parsa <parypary82@gmail.com>
2023-07-22 13:58:43 +0330

    adding some component and configuring [enentId] (event detail)

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,7 +1,31 @@
+import EventContent from "@/components/event-detail/event-content";
+import EventLogistics from "@/components/event-detail/event-logistics";
+import EventSummary from "@/components/event-detail/event-summary";
+import { getEventById } from "@/dummy-data";
+import { useRouter } from "next/router";
+
 export default () => {
+  const router = useRouter();
+  const { eventId } = router.query;
+
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return <p>No event found!</p>;
+  }
+
   return (
     <>
-      <h1>This is event detail page</h1>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
     </>
   );
 };
